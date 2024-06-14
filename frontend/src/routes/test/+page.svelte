982d1f922c0c62be85efe504884bbf164661186e
Tourbut <42790398+tourbut@users.noreply.github.com>
2024-06-14 17:38:19 +0900

    Update user authentication logic

---
+++ frontend/src/routes/test/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    let test = ''
+
+    function get_test() {
+        fetch("http://127.0.0.1:8000/").then((res) => {
+            res.json().then((json) => {
+                test = json.message;
+            });
+        });
+    }
+  
+    get_test();
+</script>
+
+<h1>{test}</h1>
