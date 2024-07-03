9f112a18fe7762c3404d7c6c1c72750757530aaf
Tourbut <42790398+tourbut@users.noreply.github.com>
2024-07-03 23:08:56 +0900

    Update Dockerfile and nginx configuration for frontend deployment

--- frontend/src/routes/(test)/test2/+page.server.js
+++
@@ -1,7 +0,0 @@
-export async function load() {
-    const response = await fetch('http://localhost:8000/');  // FastAPI 엔드포인트 호출
-    const data = await response.json();
-    return {
-        message: data.message
-    };
-  }
