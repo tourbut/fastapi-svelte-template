7aaefcfc95acbdc9fa41a786925f8a54a605f9f5
Tourbut <42790398+tourbut@users.noreply.github.com>
2024-07-04 17:21:01 +0900

    Update version number and add new files

---
+++ frontend/build/_app/immutable/chunks/constants.AEXHJDOu.js
@@ -0,0 +1,1 @@
+const s="Knowslog Template Web",o="http://knowslog.com:8000",t=`${o}/api/v1`;export{t as A,o as F,s as W};
---
+++ frontend/build/_app/immutable/entry/start.aSrxFOqu.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.C0FjG_4_.js";export{t as start};
